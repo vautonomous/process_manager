44fe2431820f248ef1f68d7188a2ba83ad6d5e02
MertColak <mcolak@leodrive.ai>
2022-09-07 14:16:41 +0300

    add class functions

--- include/ui_process_manager/ui_process_manager.hpp
+++ include/ui_process_manager/ui_process_manager.hpp
@@ -1,10 +1,19 @@
 #ifndef UI_PROCESS_MANAGER_HPP_
 #define UI_PROCESS_MANAGER_HPP_
 
+// Include ROS dependencies
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/u_int8.hpp>
+
+// Include Boost dependencies
 #include <boost/process.hpp>
+#include <boost/process/extend.hpp>
+
+// Include others
+#include <vector>
 
 namespace bp = boost::process;
+namespace proc_ex = bp::extend;
 
 class UIProcessManager : public rclcpp::Node {
 public:
@@ -12,16 +21,41 @@ public:
 
 private:
 
-/**
-* @brief for shutting PC down
-*/
-    void shutdownPC(int& result);
+    std::shared_ptr<bp::group> gprocess_autoware_;
+    std::vector<bp::child> vector_process_;
 
+    //!< @brief ui process manager diagnostic publisher
+    rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr pub_diagnostic_;
+    //!< @brief ui process manager coomand subscriber
+    rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr sub_process_command_;
+    /**
+    * @brief callback function for commands
+    */
+    void commandCallback(std_msgs::msg::UInt8::SharedPtr msg);
+    /**
+    * @brief for shutting PC down
+    */
+    void startAutoware(int &result);
 
-/**
-* @brief for rebooting PC
-*/
-    void rebootPC(int& result);
+    /**
+    * @brief for shutting PC down
+    */
+    void killAutoware(int &result);
+
+    /**
+    * @brief for shutting PC down
+    */
+    void restartAutoware(int &result);
+
+    /**
+    * @brief for shutting PC down
+    */
+    void shutdownPC(int &result);
+
+    /**
+    * @brief for rebooting PC
+    */
+    void rebootPC(int &result);
 };
 
 #endif //UI_PROCESS_MANAGER_HPP_
--- src/ui_process_manager.cpp
+++ src/ui_process_manager.cpp
@@ -3,6 +3,27 @@
 UIProcessManager::UIProcessManager(const std::string &node_name, const rclcpp::NodeOptions &options) : Node(node_name,
                                                                                                             options) {
 
+    // Create publisher and subscriber objects
+    pub_diagnostic_ = create_publisher<std_msgs::msg::UInt8>("ui_process_diagnostic", 1);
+    sub_process_command_ = create_subscription<std_msgs::msg::UInt8>("ui_process_command", )
+    gprocess_autoware_ = std::make_shared<bp::group>();
+}
+
+void UIProcessManager::commandCallback(std_msgs::msg::UInt8::SharedPtr msg) {
+
+}
+
+void UIProcessManager::startAutoware(int &result) {
+
+
+}
+
+void UIProcessManager::killAutoware(int &result) {
+
+}
+
+void UIProcessManager::restartAutoware(int &result) {
+
 }
 
 void UIProcessManager::shutdownPC(int &result) {
@@ -22,6 +43,12 @@ void UIProcessManager::rebootPC(int &result) {
     c.wait();
     result = c.exit_code();
 }
+
+
+
+
+
+
 
 
 
